e6b43da7e864a2f5edfcd805f5106608c2defebc
Thomas Moffet <pantherman1996@gmail.com>
2015-10-28 11:52:57 +1100

    Updated the graph so that it's not using my copy-pasta.

--- Graph.h
+++ Graph.h
@@ -18,32 +18,9 @@
 #include <stdexcept>
 
 namespace Biendeo {
-	
-#else
-#include <stdlib.h>
-#endif
 
-/// Swaps two numbers.
-void intSwap(int *int1, int *int2) {
-	int temp = *int1;
-	*int1 = *int2;
-	*int2 = temp;
 }
 
-/// Sorts an array by bubblesort.
-void bubbleSort(int *arr, int numItems) {
-	for (int upperBound = numItems; upperBound > 0; upperBound--) {
-		for (int i = 0; i < upperBound - 1; i++) {
-			if (arr[i] > arr[i + 1]) {
-				intSwap(&(arr[i]), &(arr[i + 1]));
-			}
-		}
-	}
-}
-
-
-#ifdef __cplusplus
-}
 #endif
 
 // Anything from here on out can be used in both C and C++.
